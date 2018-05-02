f897f75428c244bda1ae8caadf4ded1ea3cfdff8
yoavmmn <yoavmmn@gmail.com>
2018-05-02 20:25:19 +0300

    added login, fixed auth middlewear

--- routes/auth.js
+++ routes/auth.js
@@ -114,6 +114,71 @@ router.post('/register', async (req, res) => {
   });
 });
 
+router.post('/login', async (req, res) => {
+  const {
+    email = '',
+    password = ''
+  } = req.body;
+
+  if (!isEmail(email)) {
+    return res.json({
+      success: false,
+      error: 'Expected valid email address'
+    });
+  }
+
+  if (!password) {
+    return res.json({
+      success: false,
+      error: 'Expected password parameter'
+    });
+  }
+
+  if (password.length < 8) {
+    return res.json({
+      success: false,
+      error: 'Expected password length to be at least 8 characters'
+    });
+  }
+
+  let user;
+  try {
+    user = await User.findOne({ email });
+  } catch (e) {
+    logger.error(`Error finding user: ${e}`);
+
+    return res.status(500).json({
+      success: false,
+      error: 'Unexpected server error'
+    });
+  }
+
+  if (!user) {
+    return res.json({
+      success: false,
+      error: 'Email address or password is incorrect'
+    });
+  }
+
+  const isMatch = await user.comparePassword(password);
+
+  if (!isMatch) {
+    return res.json({
+      success: false,
+      error: 'Email address or password is incorrect'
+    });
+  }
+
+  const token = jwt.sign({
+    id: user.id
+  }, process.env.JWT_SECRET);
+
+  return res.json({
+    token,
+    success: true
+  });
+});
+
 router.use('/', async (req, res, next) => {
   if (tokenExists(req.headers)) {
     const token = getToken(req.headers);
@@ -128,7 +193,7 @@ router.use('/', async (req, res, next) => {
     if (data && data.id) {
       const { id } = data;
 
-      const user = await User.findOne({ id });
+      const user = await User.findOne({ _id: id });
 
       if (user && user.id === id) {
         return next();

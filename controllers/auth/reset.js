970adbaafdf2a6fc4e989627fd11d25a9b107ee5
yoavmmn <yoavmmn@gmail.com>
2018-05-04 17:26:49 +0300

    added initial version

---
+++ controllers/auth/reset.js
@@ -0,0 +1,37 @@
+const User = require('../../models/user');
+const logger = require('../../lib/logger');
+
+const { validateEmail } = require('../../lib/validate');
+
+const reset = async function reset(req, res) {
+  const {
+    email = ''
+  } = req.body;
+
+  const emailResponse = validateEmail(email);
+
+  if (!emailResponse.success) {
+    return res.json(emailResponse);
+  }
+
+  let user;
+  try {
+    user = await User.findOne({ email });
+  } catch (e) {
+    logger.error(`Error finding user: ${e}`);
+
+    return res.status(500).json({
+      success: false,
+      error: 'Unexpected server error'
+    });
+  }
+
+  return res.json({
+    success: true,
+    data: {
+      message: `An email has been sent to ${email} with further instructions`
+    }
+  });
+};
+
+module.exports = reset;

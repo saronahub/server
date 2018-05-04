5c5157dd4c7941298be259ef05a7f90d36655e31
yoavmmn <yoavmmn@gmail.com>
2018-05-04 03:06:10 +0300

    seperated email and password validation

--- routes/auth.js
+++ routes/auth.js
@@ -6,11 +6,13 @@ const logger = require('../lib/logger');
 const { getToken, tokenExists } = require('../lib/util');
 
 const loginController = require('../controllers/auth/login');
+const resetController = require('../controllers/auth/reset');
 const registerController = require('../controllers/auth/register');
 
 const router = Router();
 
 router.post('/login', loginController);
+router.post('/reset', resetController);
 router.post('/register', registerController);
 
 router.use('/', async (req, res, next) => {

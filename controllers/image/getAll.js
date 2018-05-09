74a9809d22231926a57e80c28770d54b1657e12a
yoavmmn <yoavmmn@gmail.com>
2018-05-09 15:30:27 +0300

    added images find, skip and sort

--- controllers/image/getAll.js
+++ controllers/image/getAll.js
@@ -1,4 +1,23 @@
+const Image = require('../../models/image');
+
 const getAll = async function getAll(req, res) {
+  const { end, start } = req.params;
+
+  const skip = Math.abs(start) || 0;
+  const limit = Math.abs(end) - skip || 10;
+
+  const images = await Image.find({}, null, {
+    skip,
+    limit,
+    sort: {
+      timestamp: -1
+    }
+  });
+
+  return res.json({
+    success: true,
+    data: images
+  });
 };
 
 module.exports = getAll;

cf95d92dabdebb5f3ea9bd2ef0dc41f9136148cd
yoavmmn <yoavmmn@gmail.com>
2018-05-30 19:29:57 +0300

    added auth middleware for all /image, POST /event endpoints

--- routes/event.js
+++ routes/event.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
 
+const authMiddleware = require('../lib/authMiddleware');
 const getAllController = require('../controllers/event/getAll');
 const newEventController = require('../controllers/event/newEvent');
 
 const router = Router();
 
+// Public routes
 router.get('/', getAllController);
+
+// Private routes, requires valid token
+router.use(authMiddleware);
 router.post('/', newEventController);
 
 module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,15 @@ const { router: visit } = require('./visit');
 const { router: event } = require('./event');
 const { router: image } = require('./image');
 
-const router = Router();
+const authMiddleware = require('../lib/authMiddleware');
 
+const router = Router();
 
 router.use('/auth', auth);
 // router.use('/user', user);
 // router.use('/visit', visit);
 router.use('/event', event);
-router.use('/image', image);
+router.use('/image', authMiddleware, image);
 
 module.exports = {
   router

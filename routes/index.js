4cc06d05362ecc36b22f414fcc94ac6400189d03
yoavmmn <yoavmmn@gmail.com>
2018-05-31 17:27:46 +0300

    moved login, register and reset routes to /user endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 
 const { router: user } = require('./user');
-const { router: auth } = require('./auth');
 const { router: visit } = require('./visit');
 const { router: event } = require('./event');
 const { router: image } = require('./image');
@@ -10,8 +9,7 @@ const authMiddleware = require('../lib/authMiddleware');
 
 const router = Router();
 
-router.use('/auth', auth);
-// router.use('/user', user);
+router.use('/user', user);
 // router.use('/visit', visit);
 router.use('/event', event);
 router.use('/image', authMiddleware, image);
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,14 @@
 const { Router } = require('express');
 
+const loginController = require('../controllers/user/login');
+const resetController = require('../controllers/user/reset');
+const registerController = require('../controllers/user/register');
+
 const router = Router();
+
+router.post('/login', loginController);
+router.post('/reset', resetController);
+router.post('/register', registerController);
 
 module.exports = {
   router

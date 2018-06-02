192944047af4c6eaf691e24ae0af5e7102b4ae7d
yoavmmn <yoavmmn@gmail.com>
2018-06-02 23:33:48 +0300

    added GET /versions endpoint that returns available api versions

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const { router: user } = require('./user');
 const { router: visit } = require('./visit');
 const { router: event } = require('./event');
 const { router: image } = require('./image');
+const { router: version } = require('./version');
 
 const authMiddleware = require('../lib/authMiddleware');
 
@@ -12,6 +13,7 @@ const router = Router();
 router.use('/user', user);
 // router.use('/visit', visit);
 router.use('/event', event);
+router.use('/versions', version);
 router.use('/image', authMiddleware, image);
 
 module.exports = {

f5d68bf3ea3cda8841057459551ffc93aa978254
yoavmmn <yoavmmn@gmail.com>
2018-07-02 18:11:53 +0300

    moved versions route to AWS Lambda

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const { router: user } = require('./user');
 const { router: visit } = require('./visit');
 const { router: event } = require('./event');
 const { router: image } = require('./image');
-const { router: version } = require('./version');
 
 const authMiddleware = require('../lib/authMiddleware');
 
@@ -13,7 +12,6 @@ const router = Router();
 router.use('/user', user);
 // router.use('/visit', visit);
 router.use('/event', event);
-router.use('/versions', version);
 router.use('/image', authMiddleware, image);
 
 module.exports = {
